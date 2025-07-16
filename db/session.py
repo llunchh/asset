87b26956e45312d9307cda9c13d172c741b8d514
rracle <haha082@naver.com>
2025-07-17 07:26:21 +0900

    change session.py : postgresql 연결

--- db/session.py
+++ db/session.py
@@ -1,8 +1,8 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///device.db"
+#DATABASE_URL = "postgresql://계정:비번@IP/db명"
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
